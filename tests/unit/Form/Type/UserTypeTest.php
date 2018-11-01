e807f28cec33ed7ad47fdde3fdae7c6d8d9bcb93
Ryan <ryanfarrah25410@gmail.com>
2018-11-01 08:36:10 +0100

    Create UserTypeTest for testing UserType form

---
+++ tests/unit/Form/Type/UserTypeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\AppBundle\Form\Type;
+
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $options = $this->createMock(OptionsResolver::class);
+
+        $options->setDefaults(array(
+            'host'     => 'lol',
+        ));
+
+        $options->resolve(array("hosst" => "loql"));
+        $formData = array(
+            'username' => 'username2',
+            'plainPassword' => 'password2',
+            'email' => 'email2'
+        );
+        $objectToCompare = new User();
+        $form = $this->factory->create(UserType::class, $objectToCompare);
+
+        $object = new User();
+        $object->setUsername('username2');
+        $object->setPlainPassword('password2');
+        $object->setEmail('email2');
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($object, $objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
