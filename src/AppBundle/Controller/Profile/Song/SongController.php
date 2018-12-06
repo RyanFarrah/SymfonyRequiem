92a206b3560617f10fcc85bb9a2ca31f26deac1f
Ryan <ryanfarrah25410@gmail.com>
2018-12-06 22:49:19 +0100

    remove EditSongController and newSongController and mix them in SongController

--- src/AppBundle/Controller/Profile/Song/SongController.php
+++ src/AppBundle/Controller/Profile/Song/SongController.php
@@ -8,12 +8,54 @@ use AppBundle\Service\FileHandler;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\File;
 
 use AppBundle\Entity\Song;
 use AppBundle\Form\Song\SongType;
 
-class NewSongController extends Controller
+class SongController extends Controller
 {
+    /**
+     * @Route("/profile/edit/{id}", name="edit_song", requirements={"id"="\d+"})
+     * @param Request
+     * @param EntityManagerInterface
+     * @param Song
+     * @param FileHandler
+     */
+    public function editAction(Request $request, EntityManagerInterface $em, Song $song, FileHandler $fileHandler)
+    {
+
+        $audioNameFile = $song->getAudioFile();
+
+        $audioPath = $this->getParameter('audio_directory');
+
+        $song->setAudioFile(
+            new File($this->getParameter('audio_directory') . $song::AUDIOFILEPATH . $song->getAudioFile()));
+
+        $form = $this->createForm(SongType::class, $song);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) { 
+
+            $song = $form->getData();
+
+            if($song->getAudioFile() !== null) {
+                unlink($audioPath . $song::AUDIOFILEPATH . $audioNameFile);
+                $audioNameFile = $fileHandler->newAudioFile($song);
+            }
+
+            $song->setAudioFile($audioNameFile);
+
+            $em->persist($song);
+            $em->flush();
+
+        }
+
+        return $this->render('profile/song/edit_song.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
     /**
      * @Route("/profile/new", name="new_song")
      * @param Request $request
