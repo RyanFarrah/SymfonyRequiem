110660ff75de5e098390cd589c3980bb8649d17a
Ryan <ryanfarrah25410@gmail.com>
2019-01-06 02:08:15 +0100

    SongTest : correct wrong information, test creation of a Song

--- tests/unit/AppBundle/Entity/SongTest.php
+++ tests/unit/AppBundle/Entity/SongTest.php
@@ -5,6 +5,7 @@ namespace tests\functional\AppBundle\Entity;
 use AppBundle\Entity\Song;
 use Symfony\Component\HttpFoundation\File\File;
 use PHPUnit\Framework\TestCase;
+use AppBundle\Entity\User;
 
 class SongTest extends TestCase
 {
@@ -12,25 +13,25 @@ class SongTest extends TestCase
     {
         $song = new Song;
 
-        $pathAudioFile = getcwd() . '/app/shared/files/test/uploads/audio/cover/cover.jpg';
+        $pathAudioFile = getcwd() . '/app/shared/files/test/uploads/audio/file/about_a_girl.mp3';
         $audioFile = new File($pathAudioFile);
 
         $song->setAudioFile($audioFile);
         $song->transformAudioFileToModelData();
 
-        $this->assertEquals('cover.jpg', $song->getAudioFile());
+        $this->assertEquals('about_a_girl.mp3', $song->getAudioFile());
     }
 
     public function testTransformAudioFileToModelDataWhenModelData()
     {
         $song = new Song;
 
-        $nameAudioFile = 'cover.jpg';
+        $nameAudioFile = 'about_a_girl.mp3';
 
         $song->setAudioFile($nameAudioFile);
         $song->transformAudioFileToModelData();
 
-        $this->assertEquals('cover.jpg', $song->getAudioFile());
+        $this->assertEquals('about_a_girl.mp3', $song->getAudioFile());
     }
 
     public function testTransformAudioFileToModelDataFails()
@@ -45,5 +46,27 @@ class SongTest extends TestCase
         $song->setAudioFile($wrongData);
         $song->transformAudioFileToModelData();
 
+    }
+
+    public function testNewSong()
+    {
+        $song = new Song();
+
+        $user = $this->createMock(User::class);
+        $now = new \DateTime();
+
+        $song->setAudioFile('song.mp3');
+        $song->setCover('cover.jpg');
+        $song->setAudioName('song');
+        $song->setCreatedAt($now);
+        $song->setUpdatedAt($now);
+        $song->setUser($user);
+
+        $this->assertEquals('song.mp3', $song->getAudioFile());
+        $this->assertEquals('cover.jpg', $song->getCover());
+        $this->assertEquals('song', $song->getAudioName());
+        $this->assertEquals($now, $song->getCreatedAt());
+        $this->assertEquals($now, $song->getUpdatedAt());
+        $this->assertEquals($user, $song->getUser());
     }
 }
